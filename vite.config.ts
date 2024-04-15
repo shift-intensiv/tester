937abad47be4999670fe4744aa6706a90daf23a8
debabin <dmitriybabinup@gmail.com>
2024-04-15 17:49:08 +0700

    main 🧊 fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import url from 'node:url';
 import { defineConfig } from 'vite';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
