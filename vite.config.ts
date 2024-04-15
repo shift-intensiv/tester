3579695f665a7f91ec924111f7a3a401a6f8726e
debabin <dmitriybabinup@gmail.com>
2024-04-15 17:45:12 +0700

    main 🧊 fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 
 const __filename = fileURLToPath(import.meta.url);
