38430fb199c9964659b7370f89d4802184c326bb
debabin <dmitriybabinup@gmail.com>
2024-04-21 23:49:12 +0700

    main 🧊 return loading

--- src/modules/auth/view.tsx
+++ src/modules/auth/view.tsx
@@ -56,7 +56,7 @@ export const AuthView = () => {
         )}
 
         <div className={styles.button_container}>
-          <Button type='submit' loading disabled>
+          <Button type='submit' loading={state.isLoading}>
             Войти
           </Button>
 
